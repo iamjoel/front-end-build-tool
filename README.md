28d59d049e7c69698f1818ca2f15a3fc3ef7d9ee
joel <iamjoel007@gmail.com>
2015-03-12 11:10:39 +0800

    fin version 0.0.1

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# gulp workflow
+# 前端构建工具
+基于[Gulp](http://gulpjs.com/)的前端构建工具
 
-https://github.com/gulpjs/gulp/issues/800
+## 启动
+1. 下载项目
+1. 安装 [Nodejs](http://nodejs.org/)
+1. 安装 [Gulp](http://gulpjs.com/)
+	* `npm install -g gulp`
+1. 安装项目依赖
+	* `cd 项目根目录`
+	* `npm install`
+
+## 目录结构
+* `src` 源文件
+	* `src/asserts` js,css等资源文件
+	* `src/views` html文件
+* `bulid` 构建后的文件
+
+## 支持功能列表
+* [脚本压缩](#js-min)
+* [Liveload](#liveload) 页面的css，js，html发生改变时，主动的刷新页面
+
+## 待支持的功能
+* CSS,img的压缩
+* 文件和合并
+* 路径的替换：将文件中所有的开发路径和替换成发布路径
+* Liveload 和静态服务器整合到一起
+
+## <a name="js-min">脚本压缩</a>
+执行 `gulp js-min`
+
+## <a name="liveload">Liveload</a>
+使用此功能要安装Chrome插件,[点我安装](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei)。
+执行    
+1. `gulp watch`
+1. 用静态服务器打开页面。推荐用使用[anywhere](https://www.npmjs.com/package/anywhere)
+1. 在页面上启用Liveload的Chrome插件
+![enable-gulp-livereload](doc/asserts/img/gulp-livereload.gif)
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,16 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+// 用livereload 要装Chrome插件
+// https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
+var livereload = require('gulp-livereload');
+
+var SRC_PATH = 'src';
+var DIST_PATH = 'build';
 
 // 清空目标文件夹的内容
 gulp.task('remove', function() {
-    return gulp.src('dist/**/*', {
+    return gulp.src(DIST_PATH + '/**/*', {
             read: false
         })
         .pipe(clean());
@@ -13,15 +19,22 @@ gulp.task('remove', function() {
 
 // 压缩
 gulp.task('js-min', function() {
-    return gulp.src('asserts/**/*.js')
+    return gulp.src(SRC_PATH + '/**/*.js')
         .pipe(uglify()) // 压缩
         .pipe(rename({ // 压缩后的文件加后缀
             suffix: ".min"
         }))
-        .pipe(gulp.dest('dist'));// 移动到目标文件。若目标文件夹不存在，会自动创建
+        .pipe(gulp.dest(DIST_PATH)); // 移动到目标文件。若目标文件夹不存在，会自动创建
 });
 
-// https://cnodejs.org/topic/53427d16dc556e3b3901861e
-// live load
+// liveload 页面的css，js，html发生改变时，主动的刷新页面
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch(SRC_PATH + '/**/*', function (path) {
+        // console.info('flie changed');
+        livereload.changed(path);// 通知浏览器刷新页面
+    });
+});
 
 gulp.task('build', ['remove', 'js-min']);
+gulp.task('w', ['watch']);
